6a4ba7e967f0f040714da18b02cd11388f5d77e1
David Bravo Cisterna <bravocisterna@gmail.com>
2023-12-11 13:36:20 -0300

    actualizacion

--- src/components/Rrss.jsx
+++ src/components/Rrss.jsx
@@ -1,17 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub , faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 const Rrss = () => {
     return (
         <>
         <div>
-            <a href="mailto:mariano@laboraljuridico.com"><FontAwesomeIcon icon={faEnvelope} className='icon' /></a>
-        </div>
-        <div>
-            <a href="https://github.com/dbravoc"><FontAwesomeIcon icon={faFacebook} className='icon' /></a>
-            <a href="https://github.com/dbravoc"><FontAwesomeIcon icon={faInstagram} className='icon' /></a>
+            <a href="https://github.com/dbravoc"><FontAwesomeIcon icon={faGithub} className='icon' /></a>
             <a href="https://www.linkedin.com/in/bravocisterna/"><FontAwesomeIcon icon={faLinkedin} className='icon' /></a>
         </div>
         
